9af2495b52cdc716f8039e5f5316da4771600dd0
sridharmng <sri.mangalampalli@gmail.com>
2016-01-22 13:57:28 +0530

    CoomitRoom22

---
+++ MusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace MusicStore.Models
+{
+    class MusicStoreEntities: DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+    }
+}
